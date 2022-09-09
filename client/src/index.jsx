c943396b07b2432265682cbe52d6eff940427260
Blake Crawford <blake.crawford@outlook.com>
2022-09-08 19:34:51 -0700

    added index, installed webpack, fixed bundle

---
+++ client/src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Search from './components/Search.jsx';
+import Gallery from './components/Search.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images = [];
+    }
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  search(query) {
+
+  }
+
+  render() {
+    return (<div>
+      <h1>Art by Humans</h1>
+      <Search onSearch={this.search.bind(this)}/>
+      <Gallery images={this.state.images}/>
+    </div>)
+  }
+
+  ReactDOM.render(<App />, document.getElementById('app'));
+}
